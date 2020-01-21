b4992f747035867086db416efd8bc8c995ba5832
MinexAutomation <minex.automation@gmail.com>
2020-01-21 12:38:33 -0800

    Added IOrganizationDirectoryNameConvention extensions.

---
+++ source/R5T.Egaleo.Extensions/Code/Extensions/IOrganizationDirectoryNameConventionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using R5T.Ostrogothia;
+
+
+namespace R5T.Egaleo.Extensions
+{
+    public static class IOrganizationDirectoryNameConventionExtensions
+    {
+        public static string GetOrganizationDirectoryName(this IOrganizationDirectoryNameConvention convention, IOrganization organization)
+        {
+            var organizationDirectoryName = convention.GetOrganizationDirectoryName(organization.Name);
+            return organizationDirectoryName;
+        }
+    }
+}
